b5844e513a8ca54e2ba52d6dc6b3e5b4caf3fcf4
Hiroyoshi Kurohara <hkurohar@insight-tec.co.jp>
2013-12-05 15:30:09 +0700

    We can use another convinience funcion, it is better

--- sqlparsetest/gdatest.c
+++ sqlparsetest/gdatest.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include <sql-parser/gda-sql-parser.h>
 
-
+/**
+ * main
+ */
 int main(int argc, char ** argv) {
+  int abc = NULL;
+  int inbufsize = 4096;
+  char inbuf[inbufsize];
+  int readsize = 0;
   GdaSqlParser * pParser = NULL;
   GdaStatement * pStatement = NULL;
+  GError *pError = NULL;
+  GdaSqlStatement *pSqlStatement = NULL;
+  /* read stdin as many characters as I can hold. */
+  readsize = fread(inbuf, 1, inbufsize - 1, stdin);
+  inbuf[ readsize ] = '\0';
+  /* Do parse SQL */
   pParser = gda_sql_parser_new();
-  pStatement = gda_sql_parser_parse_string(pParser, "select * from information_schema.tables where engine = 'InnoDB'", NULL, NULL);
-  fprintf(stdout, "%s\n", gda_sql_statement_type_to_string(gda_statement_get_statement_type(pStatement)));
+  pStatement = gda_sql_parser_parse_string(pParser, inbuf, NULL, NULL);
+  //Serialize to json
+  printf("%s",gda_statement_serialize(pStatement));
+ 
   return 0;
 }
 
