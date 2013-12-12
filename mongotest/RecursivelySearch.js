ede1f00098e627c2b28d164128bca6a26f5ff707
nguyenxuanluong-luvina <luongnx512@gmail.com>
2013-12-12 14:43:32 +0700

    Update comparator

--- mongotest/RecursivelySearch.js
+++ mongotest/RecursivelySearch.js
@@ -1,46 +1,20 @@
-//Define functions to compare key and value
-var eq = function(doc,key,value) { if (doc[key] === value) return true; else return false }
-var not_eq = function(doc,key,value) { if (doc[key] !== value) return true; else  return false }
-var gt = function(doc,key,value) { if (doc[key] > value) return true; else return false }
-var lt = function(doc,key,value) { if (doc[key] < value) return true; else return false }
-var gt_eq = function(doc,key,value) { if (doc[key] >= value) return true; else return false }
-var lt_eq = function(doc,key,value) { if (doc[key] <= value) return true; else return false }
 //Function to recusive search 
-function find_recursive(doc, key, value, comOperator) {
+function find_recursive(doc,comparator) {
     for (var attr in doc) {
-        if ((typeof doc[key] != "undefined") && (typeof comOperator != "undefined" ) && comOperator !== "") {
-            if(comOperator.trim() == "="){
-                return eq(doc,key,value)
-            }
-            if(comOperator.trim() == "!="){
-                return not_eq(doc,key,value)
-            }
-            if(comOperator.trim() == ">"){
-                return gt.call(doc,key,value)
+            if (comparator(doc)) {
+                return true;
             }
 
-            if(comOperator.trim() == ">="){
-                return gt_eq(doc,key,value)
-            }
-
-            if(comOperator.trim() == "<"){
-                return lt(doc,key,value)
-            }
-
-            if(comOperator.trim() == "<="){
-                return lt_eq(doc,key,value)
-            }
-        }
         if (doc[attr] !== null && typeof (doc[attr]) == "object") {
             if (doc[attr] instanceof Array) {
                 for (var i = 0; i < doc[attr].length; i++) {
-                    var result = find_recursive(doc[attr][i], key, value, comOperator);
+                    var result = find_recursive(doc[attr][i],comparator);
                     if (result) {
                         return result
                     }
                 }
             } else {
-                var result = find_recursive(doc[attr], key, value, comOperator);
+                var result = find_recursive(doc[attr], comparator);
                 if (result) {
                     return result
                 }
@@ -48,14 +22,13 @@ function find_recursive(doc, key, value, comOperator) {
         }
     }
 }
-/*
-// Parameter : key , value and comparision operator passing to search function
-var key = "join_type"
-var value = "INNER"
-var compOperator = "="
+/* Usage :
+//Define functions to compare key and value
+var comparator = function(doc) { if (doc["join_type"] == "INNER") return true; else return false }
 
+//Query usign comparator and search function
 db['sqltext_parsed_test'].find().forEach( function( doc){
- if(find_recursive(doc,key,value,compOperator)){
+ if(find_recursive(doc,comparator)){
  printjson(doc.statement.sql)
  print("-------------------------")
  }
